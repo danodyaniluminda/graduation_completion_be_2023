7b4d6120d8a8e3427ff69a11d00eeb1c81431a1c
user <klsiw@ou.ac.lk>
2023-07-11 09:09:25 +0530

    'graduation_be'

---
+++ src/main/java/com/ousl/graduation_completion/GraduationCompletionApplication.java
@@ -0,0 +1,13 @@
+package com.ousl.graduation_completion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraduationCompletionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraduationCompletionApplication.class, args);
+	}
+
+}
