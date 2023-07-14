a43e5702e8d992d941b0728f2ba59c2ce2eb46e8
Pramod <apuda@ou.ac.lk>
2023-07-14 11:23:10 +0530

    recreate format code style

--- src/main/java/com/ousl/graduation_completion/controller/TestController.java
+++ src/main/java/com/ousl/graduation_completion/controller/TestController.java
@@ -39,34 +39,34 @@ public class TestController {
         return "pramod";
     }
 
-    @Transactional
-    @GetMapping("/checkCntCourse")
-    public ResponseEntity<?> checkCntCourse(@RequestParam("id") Integer progid){
-        String sql = "select distinct course_id, application_id from student where course_type= 3 and program_id = "+progid+" and valid=false  and course_id not in (select program_cnt_courses.course_id from program_cnt_courses where program_cnt_courses.program_id="+progid+" )";
-        Query query = em.createNativeQuery(sql);
-        List<Object[]> results = query.getResultList();
-        DataObject dataObject = new DataObject();
-        if(!results.isEmpty()){
-            List<ContinueCourseDTO> dataItems = new ArrayList<>();
-            for (Object[] result : results) {
-                Long  courseId = (Long) result[0];
-                Long applicationId = (Long) result[1];
-                ContinueCourseDTO dataItem = new ContinueCourseDTO();
-                dataItem.setCourseId(courseId);
-                dataItem.setApplicationId(applicationId);
-                dataItems.add(dataItem);
-            }
-            dataObject.setMessage("some courses are not valid");
-            dataObject.setData(dataItems);
-        }else{
-            String sql1 = "update student set valid=true where course_type=3 and program_id = "+progid+" and  valid=false and course_id in (select program_cnt_courses.course_id from program_cnt_courses where program_cnt_courses.program_id="+progid+" )";
-            Query query1 = em.createNativeQuery(sql1);
-            int updatedCount = query1.executeUpdate();
-            System.out.println("update count "+updatedCount);
-            dataObject.setMessage("All the Continue Courses are Considered");
-        }
-        return new ResponseEntity<>(dataObject,HttpStatus.OK);
-    }
+//    @Transactional
+//    @GetMapping("/checkCntCourse")
+//    public ResponseEntity<?> checkCntCourse(@RequestParam("id") Integer progid){
+//        String sql = "select distinct course_id, application_id from student where course_type= 3 and program_id = "+progid+" and valid=false  and course_id not in (select program_cnt_courses.course_id from program_cnt_courses where program_cnt_courses.program_id="+progid+" )";
+//        Query query = em.createNativeQuery(sql);
+//        List<Object[]> results = query.getResultList();
+//        DataObject dataObject = new DataObject();
+//        if(!results.isEmpty()){
+//            List<ContinueCourseDTO> dataItems = new ArrayList<>();
+//            for (Object[] result : results) {
+//                Long  courseId = (Long) result[0];
+//                Long applicationId = (Long) result[1];
+//                ContinueCourseDTO dataItem = new ContinueCourseDTO();
+//                dataItem.setCourseId(courseId);
+//                dataItem.setApplicationId(applicationId);
+//                dataItems.add(dataItem);
+//            }
+//            dataObject.setMessage("some courses are not valid");
+//            dataObject.setData(dataItems);
+//        }else{
+//            String sql1 = "update student set valid=true where course_type=3 and program_id = "+progid+" and  valid=false and course_id in (select program_cnt_courses.course_id from program_cnt_courses where program_cnt_courses.program_id="+progid+" )";
+//            Query query1 = em.createNativeQuery(sql1);
+//            int updatedCount = query1.executeUpdate();
+//            System.out.println("update count "+updatedCount);
+//            dataObject.setMessage("All the Continue Courses are Considered");
+//        }
+//        return new ResponseEntity<>(dataObject,HttpStatus.OK);
+//    }
 
     @Transactional
     @GetMapping("/checkCntCourse2")
@@ -76,9 +76,26 @@ public class TestController {
         Long count = (Long) query.getSingleResult();
         int numCourses = count.intValue();
 
+        String sql1 = "INSERT INTO student_failed_criteria_detail (program_id, student_id, criteria_id, status, details)\n" +
+                "SELECT program_id,application_id, 1, 'pass', NULL\n" +
+                "FROM student \n" +
+                "WHERE program_id = "+progid+" AND course_type = 3 AND valid = false AND grade_map_id >= 5\n" +
+                "GROUP BY program_id, application_id\n" +
+                "HAVING COUNT(course_id) >= "+numCourses+"";
+        Query query1 = em.createNativeQuery(sql1);
+        int updatedCount = query1.executeUpdate();
 
 
-        return new ResponseEntity<>(numCourses,HttpStatus.OK);
+        String sql2 = "INSERT INTO student_failed_criteria_detail (program_id, student_id, criteria_id, status, details)\n" +
+                "SELECT program_id,application_id, 1, 'fail', 'Not enough Continuious courses completed'\n" +
+                "FROM student\n" +
+                "WHERE program_id = "+progid+" AND course_type = 3 AND valid = false AND grade_map_id >= 5\n" +
+                "GROUP BY program_id, application_id\n" +
+                "HAVING COUNT(course_id) < "+numCourses+"";
+        Query query2 = em.createNativeQuery(sql2);
+        int updatedCount1 = query2.executeUpdate();
+
+        return new ResponseEntity<>(updatedCount,HttpStatus.OK);
     }
 //    INSERT INTO student_failed_criteria_detail (program_id, student_id, criteria_id, status, details)
 //    SELECT program_id,1, 1, 'pass', NULL
