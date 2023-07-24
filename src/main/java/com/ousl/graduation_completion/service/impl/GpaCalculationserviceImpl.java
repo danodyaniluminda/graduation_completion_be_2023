045586784677448419236f2d7fde8e4610172bf0
T.H.M Anuruddhika <thanu@ou.ac.lk>
2023-07-24 15:46:16 +0530

    updateProgramSequence

--- src/main/java/com/ousl/graduation_completion/service/impl/GpaCalculationserviceImpl.java
+++ src/main/java/com/ousl/graduation_completion/service/impl/GpaCalculationserviceImpl.java
@@ -2,6 +2,8 @@ package com.ousl.graduation_completion.service.impl;
 
 import com.ousl.graduation_completion.dto.DataObject;
 import com.ousl.graduation_completion.models.Program;
+import com.ousl.graduation_completion.models.ProgramCriterion;
+import com.ousl.graduation_completion.repository.ProgramCriterionRepository;
 import com.ousl.graduation_completion.repository.ProgramRepository;
 import com.ousl.graduation_completion.repository.StudentGpaRepository;
 import com.ousl.graduation_completion.service.GpaCalculationService;
@@ -15,6 +17,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.HashMap;
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class GpaCalculationserviceImpl implements GpaCalculationService {
@@ -23,6 +27,9 @@ public class GpaCalculationserviceImpl implements GpaCalculationService {
     StudentGpaRepository studentGpaRepository;
 
     Logger logger = LoggerFactory.getLogger(GpaCalculationserviceImpl.class);
+
+    @Autowired
+    private ProgramCriterionRepository programCriterionRepository;
 
     @PersistenceContext
     EntityManager em;
@@ -64,65 +71,100 @@ public class GpaCalculationserviceImpl implements GpaCalculationService {
 
         HashMap<String, Object> response = new HashMap<>();
         try {
-            Program program = programRepository.findById(programId).orElseThrow();
-            String sql ="DELETE FROM student_gpa WHERE program_id=:programmeId";
 
-            Query deleteQuery = em.createNativeQuery(sql);
-            deleteQuery.setParameter("programmeId",programId);
-            int executeDelete = deleteQuery.executeUpdate();
+            Optional<ProgramCriterion> programCriterion = programCriterionRepository.getProgramCriterionByProgram_IdAndActiveAndCriteria_Id(programId, true, 3L);
+
+            if (programCriterion.isPresent()) {
+
+                Program program = programRepository.findById(programId).orElseThrow();
+                String sql ="DELETE FROM student_gpa WHERE program_id=:programmeId";
+
+                Query deleteQuery = em.createNativeQuery(sql);
+                deleteQuery.setParameter("programmeId",programId);
+                int executeDelete = deleteQuery.executeUpdate();
 
 //            response.put("status", true);
 //            response.put("message", "Records deleted successfully");
 
-            if (program.getProgramType().equals("General")) {
-                String sqlGeneral = "INSERT INTO student_gpa (program_id, student_id, gpa)\n" +
-                        "SELECT program_id, application_id,\n" +
-                        "CAST(SUM(credit*gpv)/sum(credit)as decimal(7,2)) as GPA\n" +
-                        "FROM student WHERE program_id=:programmeId\n" +
-                        "AND valid=true\n" +
-                        "AND course_type <> 3\n" +
-                        "AND credit > 0\n" +
-                        "GROUP BY program_id, application_id";
+                if (program.getProgramType().equals("General")) {
+                    String sqlGeneral = "INSERT INTO student_gpa (program_id, student_id, gpa)\n" +
+                            "SELECT program_id, application_id,\n" +
+                            "CAST(SUM(credit*gpv)/sum(credit)as decimal(7,2)) as GPA\n" +
+                            "FROM student WHERE program_id=:programmeId\n" +
+                            "AND valid=true\n" +
+                            "AND course_type <> 3\n" +
+                            "AND credit > 0\n" +
+                            "GROUP BY program_id, application_id";
 
-                Query query = em.createNativeQuery(sqlGeneral);
-                query.setParameter("programmeId",programId);
-                int executeUpdate = query.executeUpdate();
+                    Query query = em.createNativeQuery(sqlGeneral);
+                    query.setParameter("programmeId",programId);
+                    int executeUpdate = query.executeUpdate();
 
 
 //                response.put("status", true);
 //                response.put("message", "Successfully updated " + executeUpdate + "no of records for General Program");
-            }
+                }
 
-            if (program.getProgramType().equals("Special")){
-                String sqlSpecial = "INSERT INTO student_gpa (program_id, student_id, gpa)\n" +
-                        "SELECT program_id,application_id,\n" +
-                        "CAST(SUM(credit*gpv*l)/(SUM(credit)*l)AS DECIMAL(7,2)) AS gpa\n" +
-                        "FROM student s, level\n" +
-                        "WHERE course_type <> 3\n" +
-                        "and s.level = level.l\n" +
-                        "AND program_id=:programmeId\n" +
-                        "AND valid=true\n" +
-                        "AND credit > 0\n" +
-                        "GROUP BY program_id, application_id, l";
+                if (program.getProgramType().equals("Special")){
+                    String sqlSpecial = "INSERT INTO student_gpa (program_id, student_id, gpa)\n" +
+                            "SELECT program_id,application_id,\n" +
+                            "CAST(SUM(credit*gpv*l)/(SUM(credit)*l)AS DECIMAL(7,2)) AS gpa\n" +
+                            "FROM student s, level\n" +
+                            "WHERE course_type <> 3\n" +
+                            "and s.level = level.l\n" +
+                            "AND program_id=:programmeId\n" +
+                            "AND valid=true\n" +
+                            "AND credit > 0\n" +
+                            "GROUP BY program_id, application_id, l";
 
-                Query query = em.createNativeQuery(sqlSpecial);
-                query.setParameter("programmeId",programId);
-                int executeUpdate = query.executeUpdate();
+                    Query query = em.createNativeQuery(sqlSpecial);
+                    query.setParameter("programmeId",programId);
+                    int executeUpdate = query.executeUpdate();
 
-                logger.info("Success ! " + executeUpdate + " number of rows updated");
+                    logger.info("Success ! " + executeUpdate + " number of rows updated");
+                    updateProgramSequence(program);
 
 //                response.put("status", true);
 //                response.put("message", "Successfully updated " + executeUpdate + " no of records for General Special");
+                }
+                return response;
             }
+            response.put("message", "Already updated.");
+            return response;
+
         } catch (Exception e) {
 
             logger.error(" Error " + e.getMessage());
+            response.put("message", "error : " + e.getMessage());
+            return response;
 //            e.printStackTrace();
 //            response.put("status", false);
 //            response.put("message", "error : " + e.getLocalizedMessage());
         }
+    }
 
-       return response;
+    private void updateProgramSequence(Program program) {
+        try {
+            List<ProgramCriterion> programCriterionList = programCriterionRepository.getAllByProgramAndActive(program, true);
+            ProgramCriterion programCriterion = programCriterionRepository.getProgramCriterionByProgramAndActive(program, true);
+
+            for (ProgramCriterion p : programCriterionList) {
+                p.setActive(false);
+                programCriterionRepository.save(p);
+            }
+
+//            ProgramCriterion newProgramCriterion = programCriterionRepository.getProgramCriterionBySequenceIdAndProgram(programCriterion.getSequenceId() + 1, program);
+//            if (newProgramCriterion != null) {
+//                newProgramCriterion.setActive(true);
+//                programCriterionRepository.save(newProgramCriterion);
+//            }
+            logger.info("Success ! Update Program Sequence");
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            logger.info("Error ! Update Program Sequence " + e.getMessage());
+        }
     }
 
 }
