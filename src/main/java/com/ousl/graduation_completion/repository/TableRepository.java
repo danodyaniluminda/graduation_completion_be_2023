4ef87c751353cb901d1fe163053ab8ad710b39c1
ashani <tosar@ou.ac.lk>
2023-07-27 11:04:54 +0530

    table download

--- src/main/java/com/ousl/graduation_completion/repository/TableRepository.java
+++ src/main/java/com/ousl/graduation_completion/repository/TableRepository.java
@@ -3,6 +3,7 @@ package com.ousl.graduation_completion.repository;
 import com.ousl.graduation_completion.models.ALLTables;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface TableRepository extends JpaRepository<ALLTables, Long> {
